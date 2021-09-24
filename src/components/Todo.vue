86c6a8b020908b94029fd94ffd0d8f87c9d403a5
sieun-Bae <sieun.bae98@gmail.com>
2021-09-24 15:56:19 +0900

    [add] clear button show only when hovered

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="todo">
-    <input @click="onClickComplete" type="checkbox" id="task" name="task" />
-    <label for="task" v-show="true">{{ task }}</label>
-    <button @click="onClickClear">x</button>
+    <div class="view">
+      <input class="toggle" @click="onClickComplete" type="checkbox" id="task" name="task" />
+      <label for="task" v-show="true">{{ task }}</label>
+    </div>
+    <button class="clear" @click="onClickClear">x</button>
   </div>
 </template>
 
@@ -39,4 +41,69 @@ export default {
   font-size: 24px;
   border-bottom: 1px solid #ededed;
 }
+/*
+
+.toggle {
+  text-align: center;
+  width: 40px;
+  height: auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  border: none;
+  -webkit-appearance: none;
+  appearance: none;
+}
+
+.toggle + label {
+  background-color: #2c3e50;
+  background-repeat: no-repeat;
+  background-position: center left;
+}
+.toggle:checked + label {
+  background-color: #42b983;
+  background-repeat: no-repeat;
+  background-position: center left;
+}
+*/
+.view {
+  display: flex;
+  align-content: center;
+}
+
+label {
+  word-break: break-all;
+  padding: 15px 15px 15px 60px;
+  display: block;
+  line-height: 1.2;
+  transition: color 0.4s;
+}
+
+.clear {
+  display: none;
+  position: absolute;
+  top: 0;
+  right: 10px;
+  bottom: 0;
+  width: 40px;
+  height: 40px;
+  margin: auto 0;
+  font-size: 30px;
+  color: olive;
+  margin-bottom: 11px;
+  background-color: transparent;
+  border-color: transparent;
+  transition: color 0.2s ease-out;
+}
+
+.view:hover + .clear {
+  display: block;
+}
+
+.clear:hover {
+  color: darkolivegreen;
+}
+
+
 </style>
